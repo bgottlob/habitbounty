46c4430199dae408b534e306b8bc95f248f4b232
Brandon Gottlob <bgottlob1@gmail.com>
2016-11-20 19:35:35 -0500

    Created Habit prototype and added moment

--- habit.js
+++ habit.js
@@ -1,6 +1,35 @@
-var Habit = module.exports = {};
+const moment = require('moment');
 
-Habit.prototype.constructor = (name, price) => {
-  
+function Habit(name, reward) {
+  if (!name) this.name = 'No Name Set';
+  else this.name = String(name);
+
+  this.reward = Number(reward);
+  if (isNaN(this.reward)) this.reward = 0;
+
+  this.log = [];
 }
 
+/* Checks whether the habit was completed today */
+Habit.prototype.isComplete = function() {
+  const now = moment();
+  console.log(this);
+  console.log(this.log);
+  return this.log.reduce((acc, current) => {
+    return acc || now.isSame(current, 'day');
+  }, false);
+};
+
+Habit.prototype.complete = function() {
+  if (!this.isComplete())
+    this.log.push(moment());
+};
+
+var hab = new Habit("blah", 2);
+hab = new Habit();
+console.log(hab);
+console.log(hab.isComplete());
+hab.complete();
+console.log(hab.isComplete());
+hab.complete();
+console.log(hab);
