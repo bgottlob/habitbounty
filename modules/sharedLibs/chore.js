31039a72f95fcfa8a10fe1bee02d5359fdb8965b
Brandon Gottlob <info@bgottlob.com>
2017-09-07 06:46:40 -0400

    Chore tests

--- modules/sharedLibs/chore.js
+++ modules/sharedLibs/chore.js
@@ -1,27 +1,74 @@
-/* Check if we're on node; if so, we need to require stuff; if not, the
- * code was imported in the html with <script> tags*/
-if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
-  require('./sharedLib.js');
+module.exports = Chore;
+require('./sharedLib.js');
+const validate = require('./validate.js');
 
+// TODO: At some point it may be necessary to have actual differences between
+// the habit proto's log entry format and that of the chore proto
+/**
+ * A single entry in the chore log. Represents an instance when a chore was
+ * completed.
+ * @typedef LogEntry
+ * @type {object}
+ * @property {string} date the date the chore was completed
+ * @property {number} amount the amount rewarded for completion of the chore
+ */
+
+/**
+ * Creates a new Chore object
+ * @constructs Chore
+ * @param {string} name the name of the new chore
+ * @param {number} amount the amount of money rewarded to the user when the
+ *   chore is completed
+ * @param {Array<LogEntry>} [log=[]] an array of completion date string and amount
+ *   pairs to represent each day the chore was completed
+ * @returns {Chore} the newly created chore
+ */
 function Chore(name, amount, log) {
-  if (!name) this.name = 'No Name Set';
+  let err = null;
+
+  if (typeof name !== 'string')
+    throw new Error('name parameter must be a valid string');
   else this.name = String(name);
 
-  this.amount = Number(amount);
-  if (isNaN(this.amount)) this.amount = 0;
+  if (err = validate.number(amount))
+    throw err;
+  else
+    this.amount = amount;
 
-  this.log = log;
-  if (!log) this.log = [];
+  if (typeof log === 'undefined')
+    this.log = [];
+  else if (!Array.isArray(log))
+    throw new Error('log must be a valid array');
+  else {
+    for (let i = 0; i < log.length; i++) {
+      if (err = validate.logEntry(log[i])) throw err;
+    }
+    this.log = log;
+  }
 }
 
-/* Checks whether the chore was completed on the day given by the
- * date array */
+/**
+ * Checks whether the chore was completed on the date given by the date string.
+ *
+ * @param {string} dateStr a string of the date to be checked for completion
+ * @returns {boolean} true if chore was completed on the given date, false if
+ *   not
+ */
 Chore.prototype.isComplete = function(dateStr) {
+  let err = validate.dateStr(dateStr);
+  if (err) throw err;
   return this.log.reduce((acc, current) => {
     return acc || current.date === dateStr;
   }, false);
 };
 
+/**
+ * Marks the chore to be completed on a given date by adding a log entry to
+ * the Chore object's log. This has no effect if the chore has already been
+ * completed on the given date.
+ *
+ * @param {string} dateStr a string of the date the chore is to be completed on
+ */
 Chore.prototype.complete = function(dateStr) {
   if (!this.isComplete(dateStr))
     this.log.push({
@@ -30,13 +77,29 @@ Chore.prototype.complete = function(dateStr) {
     });
 };
 
+/**
+ * Removes a completion entry from the Chore object's log on the given date.
+ * This has the opposite effect as Chore#complete. This has no effect if the
+ * chore was not completed on the given date.
+ *
+ * @param {string} dateStr a string of the date the chore should not be
+ *   completed
+ */
 Chore.prototype.uncomplete = function(dateStr) {
+  let err = validate.dateStr(dateStr);
+  if (err) throw err;
   this.log = this.log.filter(function (fromLog) {
     return !(fromLog.date === dateStr);
   });
 };
 
-/* Returns an object that is ready to be placed into the db as a document */
+/**
+ * Returns an object representation of a chore that is ready to be placed into
+ * the db as a document or used as a delta for updating an existing document.
+ *
+ * @returns {object} the chore data encoded into an object friendly to the
+ *   CouchDB database
+ */
 Chore.prototype.toDoc = function() {
   let res = {
     name: this.name,
@@ -52,6 +115,13 @@ Chore.prototype.toDoc = function() {
   return res;
 };
 
+/**
+ * Creates a chore object from a chore document that was present in the CouchDB
+ * database.
+ * @param {object} doc the CouchDB doc to be converted to a Chore
+ * @returns {Chore} the Chore object populated with the data from the doc
+ */
+// TODO: Throw error if doc.type isn't "chore"?
 Chore.fromDoc = function(doc) {
   if (doc.log) {
     doc.log = doc.log.map((curr) => {
@@ -63,9 +133,3 @@ Chore.fromDoc = function(doc) {
   }
   return new Chore(doc.name, doc.amount, doc.log);
 }
-
-/* Support importing into browser or node */
-if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
-  module.exports = Chore;
-else
-  window.Chore = Chore;
